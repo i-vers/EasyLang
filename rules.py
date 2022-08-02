099b9169a76318552576910aa33da610c0fd894b
iVers <98107484+i-vers@users.noreply.github.com>
2022-08-02 22:57:14 +0300

    Improved variable system and you can now use them

--- rules.py
+++ rules.py
@@ -1,24 +1,29 @@
-filename = open('code.el', 'r')
+import re
 
+filename = open('code.el', 'r')
 variables = {}
 
 for line, command in enumerate(filename, 1):
      command = command.strip()
 
-     # Comments 
+     # Comments
      if command.startswith('-') or not command.strip():
           pass
 
      elif command.casefold().startswith('var '):
-          command = command[4:]
-          variable_name, variable_value = command.replace(" ", "").split("=")
+          variable_name, variable_value = command[4:].replace(" ", "").split("=")
           variables[variable_name.strip()] = variable_value.strip()
 
      elif command.casefold().startswith('say '):
-          if command[:4].strip() not in variables.keys():
-               print(command[4:].encode('raw_unicode_escape').decode('unicode_escape'))
-          else:
-               print(variables[command[:4].strip()])
+          variable_checker = re.findall(re.compile(r"{(\w+)}"), command)
+
+          if variable_checker:
+              for var in variable_checker:
+                   print(variables[var])
+
+          elif command.casefold().startswith('say '):
+              print(command[4:].encode('raw_unicode_escape').decode('unicode_escape'))
+
      else:
           if command.casefold() == 'say':
                print('There\'s nothing to say! If you want to make a new line use the "\\n" method.')
