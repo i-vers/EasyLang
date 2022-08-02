4c94a4fc11aac33388fd3a8bba329a26147dc1ca
iVers <98107484+i-vers@users.noreply.github.com>
2022-08-02 19:06:42 +0300

    Add files via upload

---
+++ code.el
@@ -0,0 +1,7 @@
+- This is my Hello World Program.
+
+say Hello World!\n
+say My language is so cool.
+say isnt it?
+
+var test = f
---
+++ rules.py
@@ -0,0 +1,24 @@
+filename = open('code.el', 'r')
+line = 0
+
+for line, command in enumerate(filename, 1):
+     command = command.strip()
+
+     # Comments 
+     if command.startswith('-') or not command.strip():
+          pass
+
+     elif command.casefold().startswith('var '):
+          command = command[4:]
+          print(command)
+
+     elif command.casefold().startswith('say '):
+          print(command[4:].encode('raw_unicode_escape').decode('unicode_escape'))
+  
+   
+     else:
+          if command == 'say'.casefold():
+               print('There\'s nothing to say! If you want to make a new line use the "\\n" method.')
+
+          else:
+               print(f'On line {line}, command was not recognised')
